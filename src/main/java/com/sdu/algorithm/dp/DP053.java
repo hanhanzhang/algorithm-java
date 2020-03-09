fd77ad186f72989c1498621331228524531c7f2e
zhanghan13 <zhanghan13@meituan.com>
2020-03-09 18:35:11 +0800

    动态规划

---
+++ src/main/java/com/sdu/algorithm/dp/DP053.java
@@ -0,0 +1,46 @@
+package com.sdu.algorithm.dp;
+
+
+/**
+ * 给定一个整数数组 nums, 找到一个具有最大和的连续子数组(子数组最少包含一个元素), 返回其最大和
+ *
+ * 示例:
+ *
+ * 输入: [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+ * 输出: 6
+ * 解释: 连续子数组 [4, -1, 2, 1] 的和最大为 6
+ *
+ * */
+public class DP053 {
+
+  private static int maxSubArray(int[] nums) {
+    // f[i]表是以i结尾的最大连续子数组和值
+    // f[i] = Math.max(f[i - 1] + a[i], a[i])
+
+    int[] f = new int[nums.length];
+    f[0] = nums[0];
+
+    int max = f[0];
+    for (int i = 1; i < nums.length; ++i) {
+      if (nums[i] + f[i - 1] > nums[i]) {
+        f[i] = nums[i] + f[i - 1];
+      } else {
+        f[i] = nums[i];
+      }
+
+      if (max < f[i]) {
+        max = f[i];
+      }
+    }
+
+    return max;
+  }
+
+  public static void main(String[] args) {
+
+    int[] nums = new int[] {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+    System.out.println(maxSubArray(nums));
+  }
+
+}
