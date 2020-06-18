69e5920829307fe5166cedf980d375cf03edbcdb
zhanghan13 <zhanghan13@meituan.com>
2020-06-19 07:40:37 +0800

    剑指Offer

--- src/main/java/com/sdu/algorithm/offercode/JZ020.java
+++ src/main/java/com/sdu/algorithm/offercode/JZ020.java
@@ -1,5 +1,32 @@
 package com.sdu.algorithm.offercode;
 
+import java.util.Stack;
+
 public class JZ020 {
+
+  private static Stack<Integer> stack = new Stack<>();
+  private static Stack<Integer> minStack = new Stack<>();
+  private static int min = Integer.MAX_VALUE;
+
+  private static void push(int node) {
+    stack.push(node);
+    if (node < min) {
+      min = node;
+    }
+    minStack.push(min);
+  }
+
+  private static void pop() {
+    stack.pop();
+    minStack.pop();
+  }
+
+  private static int top() {
+    return stack.peek();
+  }
+
+  private static int min() {
+    return minStack.peek();
+  }
 
 }
