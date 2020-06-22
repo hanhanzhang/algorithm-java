aa36f1c68551d71bae5c4f929d74e8df4dbfcd03
zhanghan13 <zhanghan13@meituan.com>
2020-06-22 10:46:02 +0800

    剑指Offer

--- src/main/java/com/sdu/algorithm/offercode/JZ036.java
+++ src/main/java/com/sdu/algorithm/offercode/JZ036.java
@@ -1,5 +1,17 @@
 package com.sdu.algorithm.offercode;
 
+import com.sdu.algorithm.utils.ListNode;
+
 public class JZ036 {
+
+  private static ListNode FindFirstCommonNode(ListNode pHead1, ListNode pHead2) {
+    ListNode a = pHead1;
+    ListNode b = pHead2;
+    while (a != b) {
+      a = a != null ? a.next : pHead1;
+      b = b != null ? b.next : pHead2;
+    }
+    return a;
+  }
 
 }
