a491dac02c066dccba090bcbc503cfbea1531363
zhanghan13 <zhanghan13@meituan.com>
2020-06-25 18:31:01 +0800

    剑指Offer

--- src/main/java/com/sdu/algorithm/offercode/JZ065.java
+++ src/main/java/com/sdu/algorithm/offercode/JZ065.java
@@ -1,49 +1,48 @@
 package com.sdu.algorithm.offercode;
 
-import java.util.ArrayDeque;
-import java.util.Queue;
-
 public class JZ065 {
 
-  private static boolean dfs(char[][]matrix, int i, int j, int[][] visited, String word, int pos) {
-    if (i < 0 || i > matrix.length - 1 || j < 0 || j > matrix[0].length - 1 || visited[i][j] == 1) {
-      return false;
-    }
-    if (pos >= word.length()) {
+  private static boolean hasPath(char[][]matrix, int i, int j, char[] str, int pos, int[][] visited) {
+    if (pos >= str.length) {
       return true;
     }
-    if (matrix[i][j] != word.charAt(pos)) {
+    if (i < 0 || i > matrix.length - 1 || j < 0 || j > matrix[0].length - 1
+        || visited[i][j] == 1 || matrix[i][j] != str[pos]) {
       return false;
     }
     visited[i][j] = 1;
-    boolean ans = dfs(matrix, i - 1, j, visited, word, pos + 1)
-               || dfs(matrix, i + 1, j, visited, word, pos + 1)
-               || dfs(matrix, i, j - 1, visited, word, pos + 1)
-               || dfs(matrix, i, j + 1, visited, word, pos + 1);
+    boolean ans = hasPath(matrix, i - 1, j, str, pos + 1, visited)
+        || hasPath(matrix, i + 1, j, str, pos + 1, visited)
+        || hasPath(matrix, i, j - 1, str, pos + 1, visited)
+        || hasPath(matrix, i, j + 1, str, pos + 1, visited);
     // 回溯
-    visited[i][j] = ans ? 1 : 0;
+    if (!ans) {
+      visited[i][j] = 0;
+    }
+
     return ans;
   }
 
-  private static boolean hasPath(char[][] matrix, String word) {
-    if (word == null || word.isEmpty()) {
+  private static boolean hasPath(char[] matrix, int rows, int cols, char[] str) {
+    if (matrix == null || matrix.length == 0 || matrix.length < str.length) {
       return false;
     }
-    char c = word.charAt(0);
-    Queue<int[]> queue = new ArrayDeque<>();
-    for (int i = 0; i < matrix.length; ++i) {
-      for (int j = 0; j < matrix[i].length; ++j) {
-        if (matrix[i][j] == c) {
-          queue.offer(new int[] {i, j});
-        }
+    char[][] mat = new char[rows][cols];
+    int m = 0;
+    for (int i = 0; i < rows; ++i) {
+      for (int j = 0; j < cols; ++j) {
+        mat[i][j] = matrix[m++];
       }
     }
 
-    int[][] visited = new int[matrix.length][matrix[0].length];
-    while (!queue.isEmpty()) {
-      int[] start = queue.poll();
-      if (dfs(matrix, start[0], start[1], visited, word, 0)) {
-        return true;
+    for (int i = 0; i < rows; ++i) {
+      for (int j = 0; j < cols; ++j) {
+        if (mat[i][j] == str[0]) {
+          boolean isOk = hasPath(mat, i, j, str, 0, new int[rows][cols]);
+          if (isOk) {
+            return true;
+          }
+        }
       }
     }
 
@@ -51,15 +50,9 @@ public class JZ065 {
   }
 
   public static void main(String[] args) {
-
-    char[][] matrix = new char[][] {
-        {'a', 'b', 'c', 'e'},
-        {'s', 'f', 'c', 's'},
-        {'a', 'd', 'e', 'e'}
-    };
-
-    System.out.println(hasPath(matrix, "bcced"));
-    System.out.println(hasPath(matrix, "abcd"));
+    char[] matrix = "abcesfcsadee".toCharArray();
+    System.out.println(hasPath(matrix, 3, 4, "bcced".toCharArray()));
+    System.out.println(hasPath(matrix, 3, 4, "abcb".toCharArray()));
   }
 
 }
