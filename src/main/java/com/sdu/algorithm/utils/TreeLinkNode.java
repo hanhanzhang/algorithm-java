4ac7b053641dedf747250e76c0afdd603b9027bf
zhanghan13 <zhanghan13@meituan.com>
2020-06-25 19:17:32 +0800

    工具类

--- src/main/java/com/sdu/algorithm/utils/TreeLinkNode.java
+++ src/main/java/com/sdu/algorithm/utils/TreeLinkNode.java
@@ -2,4 +2,13 @@ package com.sdu.algorithm.utils;
 
 public class TreeLinkNode {
 
+  public int val;
+  public TreeLinkNode left = null;
+  public TreeLinkNode right = null;
+  public TreeLinkNode next = null;
+
+  public TreeLinkNode(int val) {
+    this.val = val;
+  }
+
 }
