1d73e79ca030cefd63c3987d0948e507870415e0
zhanghan13 <zhanghan13@meituan.com>
2020-06-25 19:28:15 +0800

    剑指Offer

--- src/main/java/com/sdu/algorithm/offercode/JZ039.java
+++ src/main/java/com/sdu/algorithm/offercode/JZ039.java
@@ -1,5 +1,48 @@
 package com.sdu.algorithm.offercode;
 
+import com.sdu.algorithm.utils.TreeNode;
+import java.util.HashMap;
+import java.util.Map;
+
 public class JZ039 {
+
+  private static int treeHeight(TreeNode root, Map<TreeNode, Integer> heights) {
+    if (root == null) {
+      return 0;
+    }
+    if (heights.containsKey(root)) {
+      return heights.get(root);
+    }
+
+    int left = treeHeight(root.left, heights) + 1;
+    int right = treeHeight(root.right, heights) + 1;
+    heights.put(root, Math.max(left, right));
+    return heights.get(root);
+  }
+
+  private static boolean IsBalanced_Solution(TreeNode root, Map<TreeNode, Integer> heights) {
+    if (root == null) {
+      return true;
+    }
+    int left = treeHeight(root.left, heights);
+    int right = treeHeight(root.right, heights);
+    if (Math.abs(left - right) > 1) {
+      return false;
+    }
+
+    return IsBalanced_Solution(root.left, heights) && IsBalanced_Solution(root.right, heights);
+  }
+
+  private static boolean IsBalanced_Solution(TreeNode root) {
+    if (root == null) {
+      return true;
+    }
+
+    return IsBalanced_Solution(root, new HashMap<>());
+  }
+
+  public static void main(String[] args) {
+
+  }
 
 }
