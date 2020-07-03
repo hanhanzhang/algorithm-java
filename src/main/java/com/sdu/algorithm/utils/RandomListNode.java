4512e4d2a47781fbc1b8cca001f1b71c6e435c6c
zhanghan13 <zhanghan13@meituan.com>
2020-07-03 10:22:59 +0800

    剑指Offer

---
+++ src/main/java/com/sdu/algorithm/utils/RandomListNode.java
@@ -0,0 +1,15 @@
+package com.sdu.algorithm.utils;
+
+public class RandomListNode {
+
+  public int label;
+
+  public RandomListNode next = null;
+
+  public RandomListNode random = null;
+
+  public RandomListNode(int label) {
+    this.label = label;
+  }
+
+}
