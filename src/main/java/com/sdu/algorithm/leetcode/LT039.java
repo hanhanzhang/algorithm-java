9ba03d80d74215e4ebce0f0796798d47213d7243
zhanghan13 <zhanghan13@meituan.com>
2020-07-04 16:12:57 +0800

    Leetcode(组合问题, 核心点: 去重)

--- src/main/java/com/sdu/algorithm/leetcode/LT039.java
+++ src/main/java/com/sdu/algorithm/leetcode/LT039.java
@@ -6,21 +6,23 @@ import java.util.List;
 
 public class LT039 {
 
-  private static List<List<Integer>> combinationSum(int[] candidates, int target) {
-    // TODO: 如何去重(做减法, 转为多叉树)
-    if (target < 0) return Collections.emptyList();
+  // start位置用于去重, 表示start前的元素不可再次被选择
+  private static List<List<Integer>> combinationSum(int[] candidates, int start, int target) {
+    if (target < 0) {
+      return null;
+    }
 
     List<List<Integer>> result = new LinkedList<>();
-
-    for (int i = 0; i < candidates.length; ++i) {
+    for (int i = start; i < candidates.length; ++i) {
       int newTarget = target - candidates[i];
       if (newTarget == 0)  {
         List<Integer> sub = new LinkedList<>();
         sub.add(candidates[i]);
         result.add(sub);
       } else {
-        List<List<Integer>> subResult = combinationSum(candidates, newTarget);
-        if (subResult.isEmpty()) {
+        // 当前节点可用多次, 故从i处开始搜索, i之前的元素表示不可再选择
+        List<List<Integer>> subResult = combinationSum(candidates, i, newTarget);
+        if (subResult == null) {
           continue;
         }
         for (List<Integer> sub : subResult) {
@@ -31,6 +33,13 @@ public class LT039 {
     }
 
     return result;
+  }
+
+  private static List<List<Integer>> combinationSum(int[] candidates, int target) {
+    if (candidates == null || candidates.length == 0 || target < 0) {
+      return Collections.emptyList();
+    }
+    return combinationSum(candidates, 0, target);
   }
 
   public static void main(String[] args) {
