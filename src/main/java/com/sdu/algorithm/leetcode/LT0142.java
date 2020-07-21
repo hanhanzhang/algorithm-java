faee93f42aa58c1a43e7cf630be44450ea89ef7a
hanhan.zhang <hanzhang800418@163.com>
2020-07-21 08:55:45 +0800

    Leetcode(环形链表 II)

--- src/main/java/com/sdu/algorithm/leetcode/LT0142.java
+++ src/main/java/com/sdu/algorithm/leetcode/LT0142.java
@@ -2,31 +2,51 @@ package com.sdu.algorithm.leetcode;
 
 import com.sdu.algorithm.utils.ListNode;
 
-public class LT142 {
+import java.util.HashSet;
+import java.util.Set;
 
-  private static ListNode detectCycle(ListNode head) {
-    if (head == null || head.next == null) {
+public class LT0142 {
+
+  public static class Solution1 {
+
+    private static ListNode detectCycle(ListNode head) {
+      // 哈希法(效率低)
+      Set<ListNode> nodes = new HashSet<>();
+      while (head != null) {
+        if (nodes.contains(head)) {
+          return head;
+        }
+        nodes.add(head);
+        head = head.next;
+      }
       return null;
     }
 
-    // 快慢指针
-    ListNode low = head.next, quick = head.next.next;
-    while (low != quick) {
-      if (low == null || quick == null || quick.next == null) {
-        return null;
+  }
+
+  public static class Solution2 {
+
+    private static ListNode detectCycle(ListNode head) {
+      ListNode slow = head;
+      ListNode fast = head;
+      do {
+        if (slow == null || fast == null || fast.next == null) {
+          return null;
+        }
+        slow = slow.next;
+        fast = fast.next.next;
+      } while (slow != fast);
+      slow = head;
+      while (slow != fast) {
+        slow = slow.next;
+        fast = fast.next;
       }
-      low = low.next;
-      quick = quick.next.next;
+      return slow;
     }
 
-    //
-    quick = head;
-    while (quick != low) {
-      quick = quick.next;
-      low = low.next;
-    }
-    return quick;
   }
+
+
 
   public static void main(String[] args) {
     ListNode root = new ListNode(3);
@@ -41,7 +61,8 @@ public class LT142 {
     node0.next = node4;
     node4.next = node2;
 
-    System.out.println(detectCycle(root));
+    System.out.println(Solution2.detectCycle(root));
+    System.out.println(Solution1.detectCycle(root));
   }
 
 }
