4ed4469ce345c54afd11b21c904123b38e6fcd47
zhanghan13 <zhanghan13@meituan.com>
2020-08-01 16:33:47 +0800

    剑指Offer(二进制中1的个数)

--- src/main/java/com/sdu/algorithm/offercode/JZ011.java
+++ src/main/java/com/sdu/algorithm/offercode/JZ011.java
@@ -5,6 +5,8 @@ public class JZ011 {
   private static int NumberOf1(int n) {
     int mark = 1;
     int ans = 0;
+    // 左移: 低位补零
+    // 右移: 正数补零, 负数补1
     while (mark != 0) {
       if ((mark & n) == mark) {
         ans += 1;
