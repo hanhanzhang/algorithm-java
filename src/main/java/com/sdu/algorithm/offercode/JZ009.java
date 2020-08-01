075429573a40f47666be212e97600dd37a3a8340
zhanghan13 <zhanghan13@meituan.com>
2020-08-01 17:28:50 +0800

    剑指Offer(变态跳台阶)

--- src/main/java/com/sdu/algorithm/offercode/JZ009.java
+++ src/main/java/com/sdu/algorithm/offercode/JZ009.java
@@ -12,6 +12,7 @@ public class JZ009 {
   public static void main(String[] args) {
     System.out.println(JumpFloorII(1));
     System.out.println(JumpFloorII(2));
+    System.out.println(JumpFloorII(3));
   }
 
 }
