51e8e070a27e2941848d747de627d1841a17e867
zhanghan13 <zhanghan13@meituan.com>
2020-08-01 20:13:58 +0800

    剑指Offer(最小的K个数)

--- src/main/java/com/sdu/algorithm/offercode/JZ029.java
+++ src/main/java/com/sdu/algorithm/offercode/JZ029.java
@@ -6,6 +6,7 @@ import java.util.PriorityQueue;
 public class JZ029 {
 
   private static ArrayList<Integer> GetLeastNumbers_Solution(int [] input, int k) {
+    // TopN问题: 堆排序
     ArrayList<Integer> ans = new ArrayList<>();
     if (input == null || input.length == 0 || input.length < k || k == 0) {
       return ans;
