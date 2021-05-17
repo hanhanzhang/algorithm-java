084638733006e7f9d12e004d8209c6cbbc1ad1c2
hanhan.zhang <hanzhang800418@163.com>
2021-05-17 11:30:16 +0800

    algo

--- src/main/java/com/sdu/algorithm/leetcode/LT0004.java
+++ src/main/java/com/sdu/algorithm/leetcode/LT0004.java
@@ -2,12 +2,48 @@ package com.sdu.algorithm.leetcode;
 
 public class LT0004 {
 
+    private static int findKth(int[] nums1, int[] nums2, int k) {
+        int length1 = nums1.length, length2 = nums2.length;
+        int index1 = 0, index2 = 0;
+        while (true) {
+            // 边界情况
+            if (index1 == length1) {
+                return nums2[index2 + k - 1];
+            }
+            if (index2 == length2) {
+                return nums1[index1 + k - 1];
+            }
+            if (k == 1) {
+                return Math.min(nums1[index1], nums2[index2]);
+            }
+            int half = k / 2;
+            int newIndex1 = Math.min(index1 + half, length1) - 1;
+            int newIndex2 = Math.min(index2 + half, length2) - 1;
+            int pivot1 = nums1[newIndex1], pivot2 = nums2[newIndex2];
+            if (pivot1 <= pivot2) {
+                k -= (newIndex1 - index1 + 1);
+                index1 = newIndex1 + 1;
+            } else {
+                k -= (newIndex2 - index2 + 1);
+                index2 = newIndex2 + 1;
+            }
+        }
+    }
+
     private static double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        return 0;
+        int length1 = nums1.length, length2 = nums2.length;
+        int totalLength = length1 + length2;
+        if (totalLength % 2 == 1) {
+            int midIndex = totalLength / 2;
+            return findKth(nums1, nums2, midIndex + 1);
+        } else {
+            int midIndex1 = totalLength / 2 - 1, midIndex2 = totalLength / 2;
+            return (findKth(nums1, nums2, midIndex1 + 1) + findKth(nums1, nums2, midIndex2 + 1)) / 2.0;
+        }
     }
 
     public static void main(String[] args) {
-
+        System.out.println(findMedianSortedArrays(new int[] {1, 2}, new int[] {3, 4}));
     }
 
 }
